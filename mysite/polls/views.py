8a252f25865e05006e4876c5295751e4d8be7457
ibisha <akashiroakaaka@gmail.com>
2017-12-10 16:41:07 +0900

    view定義追加

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,3 +2,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    results = "You're looking at the results of question %s."
+    return HttpResponse(results % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on %s." % question_id)
