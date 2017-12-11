bbc424afeb3b87acfd716b3c8f83efc62e6d7986
ibisha <akashiroakaaka@gmail.com>
2017-12-11 23:20:58 +0900

    名前空間を定義してテンプレートを作成

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
 
 from .models import Question
 
@@ -9,7 +10,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     results = "You're looking at the results of question %s."
